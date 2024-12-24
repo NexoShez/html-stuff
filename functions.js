00676ffdab4024a7b9afc6ef7ed1ccedfb3cae35
Nexi♡ <98440899+NexoShez@users.noreply.github.com>
2024-12-23 21:06:36 -0800

    about preview 1

--- functions.js
+++ functions.js
@@ -7,6 +7,14 @@ function isMobile() {
   return check;
 };
 
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 // if (isMobile == true) {
 //   document.getElementsByClassName("topbar").classList.toggle("mobile");;
 //   alert("mobile");
@@ -45,6 +53,14 @@ function cctog() {
     x.style.display = "none";
     btn.innerHTML = "-Show Changelog-";
   }
+}
+
+function changepage(page, p2, p3) {
+  document.getElementsByClassName('on').classList = "subsegment off"
+  sleep(150)
+  document.getElementById(p2).classList = "subsegment"
+  document.getElementById(p3).classList = "subsegment"
+  document.getElementById(page).classList = "subsegment on"
 }
 
 // use this one if it is UNIVERSAL (like a footer)
@@ -172,12 +188,24 @@ document.getElementById("uni").innerHTML = `
                         d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"/>
                 </svg>
             </a>
+            
+            <a href="https://cash.app/$nexiblep" class="social-link">
+                <svg 
+                fill="#ffffff" 
+                width="800px" 
+                height="800px" 
+                viewBox="0 0 24 24" 
+                role="img">
+                    <path
+                        d="M23.59 3.47A5.1 5.1 0 0 0 20.54.42C19.23 0 18.04 0 15.62 0H8.36c-2.4 0-3.61 0-4.9.4A5.1 5.1 0 0 0 .41 3.46C0 4.76 0 5.96 0 8.36v7.27c0 2.41 0 3.6.4 4.9a5.1 5.1 0 0 0 3.05 3.05c1.3.41 2.5.41 4.9.41h7.28c2.41 0 3.61 0 4.9-.4a5.1 5.1 0 0 0 3.06-3.06c.41-1.3.41-2.5.41-4.9V8.38c0-2.41 0-3.61-.41-4.91zM17.42 8.1l-.93.93a.5.5 0 0 1-.67.01 5 5 0 0 0-3.22-1.18c-.97 0-1.94.32-1.94 1.21 0 .9 1.04 1.2 2.24 1.65 2.1.7 3.84 1.58 3.84 3.64 0 2.24-1.74 3.78-4.58 3.95l-.26 1.2a.49.49 0 0 1-.48.39H9.63l-.09-.01a.5.5 0 0 1-.38-.59l.28-1.27a6.54 6.54 0 0 1-2.88-1.57v-.01a.48.48 0 0 1 0-.68l1-.97a.49.49 0 0 1 .67 0c.91.86 2.13 1.34 3.39 1.32 1.3 0 2.17-.55 2.17-1.42 0-.87-.88-1.1-2.54-1.72-1.76-.63-3.43-1.52-3.43-3.6 0-2.42 2.01-3.6 4.39-3.71l.25-1.23a.48.48 0 0 1 .48-.38h1.78l.1.01c.26.06.43.31.37.57l-.27 1.37c.9.3 1.75.77 2.48 1.39l.02.02c.19.2.19.5 0 .68z"/>
+                </svg>
+            </a>
         </div>
 
         <div class="spacetext" id="sp" style="height: 70px;">
             <p>Copyright 2024 | NX.games</p>
             <p>Designed and built by 
-                <a href="../assets/nexi.html" class="normal" id="nohover" style="padding: 0;">
+                <a href="about.html" class="normal" id="nohover" style="padding: 0;">
                     Nexi
                 </a>
                  :3
